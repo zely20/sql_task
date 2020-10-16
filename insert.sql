b0bd3d68cdbaa3da5e13cb650a7c5cdca5c0e55b
zely20@mail.ru <Civik1984>
2020-10-16 10:15:45 +0300

    add insert

---
+++ insert.sql
@@ -0,0 +1,15 @@
+insert into company (id, company) values (1, 'Meeveo');
+insert into company (id, company) values (2, 'Photobug');
+insert into company (id, company) values (3, 'Divavu');
+insert into company (id, company) values (4, 'Thoughtstorm');
+
+insert into person (id, name, company_id) values (1, 'Maryanna', 1);
+insert into person (id, name, company_id) values (2, 'Caresse', 1);
+insert into person (id, name, company_id) values (3, 'Orion', 2);
+insert into person (id, name, company_id) values (4, 'Kenneth', 2);
+insert into person (id, name, company_id) values (5, 'Marlo', 3);
+insert into person (id, name, company_id) values (6, 'Emylee', 3);
+insert into person (id, name, company_id) values (7, 'Cybill', 4);
+insert into person (id, name, company_id) values (8, 'Francene',4);
+insert into person (id, name, company_id) values (9, 'Sigismund', 4);
+insert into person (id, name, company_id) values (10, 'Loella', 3);
