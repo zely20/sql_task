88ee7255b331ccc2cd3b5d3ba1f987d08aa26ea1
zely20@mail.ru <Civik1984>
2020-10-16 12:24:06 +0300

    add task

--- task.sql
+++ task.sql
@@ -11,3 +11,11 @@ SELECT c.name, COUNT(p.company_id) AS schet FROM
 person AS p, company AS c
 WHERE c.id = p.company_id
 GROUP BY c.name;
+
+SELECT t1.* FROM
+(select c.name, COUNT(p.company_id) AS expr1 FROM
+person AS p, company AS c
+WHERE c.id = p.company_id
+GROUP BY c.name
+ORDER BY expr1 desc ) as t1
+limit 1;
